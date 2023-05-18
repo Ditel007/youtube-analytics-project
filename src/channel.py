6ebe601a24d60b69b3cc625bd531513a547175e3
Nikita Kislykh <kislykhnik@gmail.com>
2023-05-18 23:32:05 +0500

    изменил ввод api_key

--- src/channel.py
+++ src/channel.py
@@ -3,7 +3,7 @@ import os
 from googleapiclient.discovery import build
 
 # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
-api_key = "AIzaSyD86NDls37AJwtcPgrk-YenuJgIZ6nY33I"
+api_key: str = os.getenv('YT_API_KEY')
 
 # создать специальный объект для работы с API
 youtube = build('youtube', 'v3', developerKey=api_key)
