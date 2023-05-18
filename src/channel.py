5096d0c03a07f208725488e7d2f07a3a25a37025
Nikita Kislykh <kislykhnik@gmail.com>
2023-05-18 22:01:36 +0500

    добавлена функция вывода информации о канале

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,30 @@
+import json
+import os
+
+from googleapiclient.discovery import build
+
+# YT_API_KEY скопирован из гугла и вставлен в переменные окружения
+api_key: str = os.getenv('YT_API_KEY')
+
+# создать специальный объект для работы с API
+youtube = build('youtube', 'v3', developerKey=api_key)
 
 
 class Channel:
     """Класс для ютуб-канала"""
 
-    def __init__(self, channel_id: str) -> None:
+    def __init__(self, channel_id: str):
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self._channel_id = channel_id
 
-    def print_info(self) -> None:
+    @property
+    def channel(self):
+        """Возвращает название канала."""
+        return youtube.channels()
+
+    def print_info(self):
         """Выводит в консоль информацию о канале."""
-        pass
+        channel = self.channel.list(id=self._channel_id, part='snippet,statistics').execute()
+        print(f"Название канала: {channel['items'][0]['snippet']['title']}")
+        print(f"Количество видео: {int(channel['items'][0]['statistics']['videoCount'])}")
+        print(f"Ссылка на канал: https://www.youtube.com/channel/{self._channel_id}")
