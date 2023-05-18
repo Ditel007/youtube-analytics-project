0a107e1ae19884c4f9938d5a5774d463cd62fd04
Nikita Kislykh <kislykhnik@gmail.com>
2023-05-18 23:06:38 +0500

    изменил ввод api_key программа заработала

--- src/channel.py
+++ src/channel.py
@@ -1,10 +1,9 @@
-import json
 import os
 
 from googleapiclient.discovery import build
 
 # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
-api_key: str = os.getenv('YT_API_KEY')
+api_key = "AIzaSyD86NDls37AJwtcPgrk-YenuJgIZ6nY33I"
 
 # создать специальный объект для работы с API
 youtube = build('youtube', 'v3', developerKey=api_key)
@@ -25,6 +24,4 @@ class Channel:
     def print_info(self):
         """Выводит в консоль информацию о канале."""
         channel = self.channel.list(id=self._channel_id, part='snippet,statistics').execute()
-        print(f"Название канала: {channel['items'][0]['snippet']['title']}")
-        print(f"Количество видео: {int(channel['items'][0]['statistics']['videoCount'])}")
-        print(f"Ссылка на канал: https://www.youtube.com/channel/{self._channel_id}")
+        print(channel)
